d9af228038078b2183cc35fd9dc2dcc48ea0b27b
Pablo Abratte <pabratte@gmail.com>
2018-11-01 00:23:22 -0300

    Suscribe form

--- assets/js/main.js
+++ assets/js/main.js
@@ -81,4 +81,41 @@
 
 		}
 
+		$(".ajaxForm").submit(function(e){
+			e.preventDefault();
+			var href = $(this).attr("action");
+			$.ajax({
+				type: "POST",
+				dataType: "json",
+				url: href,
+				data: $(this).serialize(),
+				success: function(response){
+					if(response.status == "success"){
+						$("input[name=suscribe]").val("");
+						toastr.success('Recibirás en tu correo electrónico las próximas novedades', '¡Suscripción exitosa!')
+					}else{
+						toastr.error("An error occured.");
+					}
+				}
+			});
+		});
+
+		toastr.options = {
+			"closeButton": false,
+			"debug": false,
+			"newestOnTop": false,
+			"progressBar": false,
+			"positionClass": "toast-top-center",
+			"preventDuplicates": false,
+			"onclick": null,
+			"showDuration": "300",
+			"hideDuration": "1000",
+			"timeOut": "5000",
+			"extendedTimeOut": "1000",
+			"showEasing": "swing",
+			"hideEasing": "linear",
+			"showMethod": "fadeIn",
+			"hideMethod": "fadeOut"
+		  }
+
 })(jQuery);
